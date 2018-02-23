dcb5235ca83e9d0e6d98615add4ccf57eaf9ba00
Noah Botimer <botimer@umich.edu>
2018-02-23 15:36:36 -0500

    Add PermissionCheck and RoleCheck
    These classes capture the authority and serve as self-evaluating rules.
    The plan is that there can be a trivial factory available to policies
    to pass in the user, resource, and authority, giving good ergonomics
    without sacrificing design. For example, a can? method on a base policy
    would create a PermissionCheck and evaluate it.

---
+++ spec/checkpoint/role_check_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'checkpoint/permission_check'
+
+RSpec.describe Checkpoint::PermissionCheck do
+  let(:user)   { double('user') }
+  let(:action) { :action }
+  let(:target) { double('target') }
+
+  subject(:check) { described_class.new(user, action, target) }
+
+  it 'returns the user' do
+    expect(check.user).to eq user
+  end
+
+  it 'returns the action' do
+    expect(check.action).to eq action
+  end
+
+  it 'returns the target' do
+    expect(check.target).to eq target
+  end
+
+  it 'rejects by default' do
+    expect(check.satisfied?).to be false
+  end
+
+  context 'when there is no matching permit' do
+    let(:authority) { double('authority', permitted?: false) }
+    subject(:check) do
+      described_class.new(user, action, target, authority: authority)
+    end
+
+    it 'is not satisifed?' do
+      expect(check.satisfied?).to be false
+    end
+  end
+
+  context 'when there is a matching permit' do
+    let(:authority) { double('authority', permitted?: true) }
+    subject(:check) do
+      described_class.new(user, action, target, authority: authority)
+    end
+
+    it 'is satisfied?' do
+      expect(check.satisfied?).to be true
+    end
+  end
+end
